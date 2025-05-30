e2bff5eef40e1a42bd6c1d95bc03950d6e43a403
pucpro <caophuc434@gmail.com>
2025-05-30 20:23:57 +0700

    a

--- Lab2/bai1/bai1/Program.cs
+++ Lab2/bai1/bai1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
 namespace bai1
 {
@@ -10,7 +7,54 @@ namespace bai1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            ArrayList arraylist = new ArrayList();
+            arraylist.Add("Item 01");
+            arraylist.Add("Item 02");
+            arraylist.Add(1);
+            arraylist.Add(2);
+
+            Console.WriteLine(arraylist.Count);
+            for (int i = 0; i < arraylist.Count; i++) 
+            {
+                Console.WriteLine(arraylist[i]);
+            }
+            arraylist.Insert(1, "Item 04");
+            ArrayList arrayList2 = new ArrayList();
+            arrayList2.Add(5);
+            arrayList2.Add("Item 6");   
+            arraylist.AddRange(arrayList2);
+            
+            arraylist.Remove(5);
+
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("Key1", "value1");
+            hashtable.Add("Key2", "value2");
+            hashtable.Add("Key3", "value3");
+            hashtable.Add("Key4", 4);
+            hashtable.Add(5, "value5");
+            //hashtable.Add("key6", "value6");
+            foreach (DictionaryEntry item in hashtable)
+            {
+                Console.WriteLine("key: {0} - value: {1}", item.Key, item.Value);
+            }
+            foreach (var key  in hashtable.Keys)
+            {
+                Console.WriteLine("key: {0}", key);
+            }
+            foreach (var value in hashtable.Keys)
+            {
+                Console.WriteLine("value: {0}", value);
+            }
+            hashtable.Remove("Key3");
+            Hashtable hashtable2 =(Hashtable) hashtable.Clone();
+            hashtable.Clear();
+            
+            SortedList  sortedList = new SortedList();
+            sortedList.Add("First", 1);
+            sortedList.Add("key", 2);
+            sortedList.Add("third", "value 3");
+            sortedList.Add(4, "value 3");
+            Console.ReadLine();
         }
     }
 }
