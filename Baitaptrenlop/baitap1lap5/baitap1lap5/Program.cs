1ca574210e90aa23991bf5585d6441631f330315
pucpro <caophuc434@gmail.com>
2025-06-13 16:09:19 +0700

    a

---
+++ Baitaptrenlop/baitap1lap5/baitap1lap5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+delegate int PhepTinh(int x); 
+
+class Program
+{
+    static int Square(int x) => x * x;
+    static int Cube(int x) => x * x * x;
+
+    static void Main()
+    {
+        PhepTinh tinhToan = Square; 
+        Console.WriteLine("Ket qua: " + tinhToan(3));
+    }
+}
---
+++ Baitaptrenlop/baitap2lap5/baitap2lap5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class NumberList
+{
+    public event EventHandler<int> NumberAdded;
+    private List<int> numbers = new List<int>();
+
+    public void AddNumber(int num)
+    {
+        numbers.Add(num);
+        NumberAdded?.Invoke(this, num);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        NumberList numList = new NumberList();
+        numList.NumberAdded += (sender, num) => Console.WriteLine($"Da them so: {num}");
+
+        numList.AddNumber(6);
+        numList.AddNumber(10);
+    }
+}
